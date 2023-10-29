b32054b51f667a6bf1a570cc769a404cdf4efc3e
3dgenerik <milesoda@gmail.com>
2023-10-29 23:29:52 +0100

    fix small changes

--- migrations/sqls/20231026114710-users-table-up.sql
+++ migrations/sqls/20231026114710-users-table-up.sql
@@ -4,36 +4,3 @@ CREATE TABLE users_table (
     last_name VARCHAR(255), 
     password VARCHAR(2048)
     );
-
-/*
-{"first_name": "Milena", "last_name": "Petrovic", "password": "milena"}
-{"first_name": "Nikola", "last_name": "Jovanovic", "password": "nikola"}
-{"first_name": "Katarina", "last_name": "Popovic", "password": "katarina"}
-{"first_name": "Jelena", "last_name": "Simic", "password": "jelena"}
-{"first_name": "Stefan", "last_name": "Kovacevic", "password": "stefan"}
-{"first_name": "Dusan", "last_name": "Vukovic", "password": "dusan"}
-{"first_name": "Anastasija", "last_name": "Milinkovic", "password": "anastasija"}
-{"first_name": "Petar", "last_name": "Stojanovic", "password": "petar"}
-{"first_name": "Aleksandra", "last_name": "Lazic", "password": "aleksandra"}
-{"first_name": "Tijana", "last_name": "Vasic", "password": "tijana"}
-{"first_name": "Mina", "last_name": "Mitic", "password": "mina"}
-{"first_name": "Luka", "last_name": "Ristic", "password": "luka"}
-{"first_name": "Jovana", "last_name": "Pavlovic", "password": "jovana"}
-{"first_name": "Nenad", "last_name": "Petrovic", "password": "nenad"}
-{"first_name": "Milica", "last_name": "Djordjevic", "password": "milica"}
-{"first_name": "Andrija", "last_name": "Nikolic", "password": "andrija"}
-{"first_name": "Sara", "last_name": "Lukic", "password": "sara"}
-{"first_name": "Ognjen", "last_name": "Markovic", "password": "ognjen"}
-{"first_name": "Dunja", "last_name": "Tomic", "password": "dunja"}
-{"first_name": "Aleksa", "last_name": "Stankovic", "password": "aleksa"}
-{"first_name": "Teodora", "last_name": "Milovanovic", "password": "teodora"}
-{"first_name": "Filip", "last_name": "Simovic", "password": "filip"}
-{"first_name": "Milan", "last_name": "Rakic", "password": "milan"}
-{"first_name": "Novak", "last_name": "Gavrilovic", "password": "novak"}
-{"first_name": "Milos", "last_name": "Nikolic", "password": "milos"}
-{"first_name": "Ivana", "last_name": "Djokovic", "password": "ivana"}
-{"first_name": "Strahinja", "last_name": "Jokic", "password": "strahinja"}
-{"first_name": "Milan", "last_name": "Maksimovic", "password": "milan"}
-{"first_name": "Hana", "last_name": "Peric", "password": "hana"}
-{"first_name": "Mihajlo", "last_name": "Zivkovic", "password": "mihajlo"}
-*/
